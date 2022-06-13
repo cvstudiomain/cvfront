8c20c04144df0be213cc287ba660e08f7ce38102
ahmedelrufai <ahmedelrufai25@gmail.com>
2022-06-13 07:40:36 +0100

    update

--- js/script.js
+++ js/script.js
@@ -3152,7 +3152,7 @@ const register = async function (user) {
   const res = await axios.post("https://app.cvstudio.io/user/register", {...user,});
   loadOnLogBtn.classList.remove("show-btn-loader")
   loadOnSignUpBtn.classList.remove("show-btn-loader")
-
+// return console.log(res)
   if (!res.data.result.MessageId) return (message2.textContent = `We are having problem verifying your account. We will notify you anytime the problem is fixed!`);
   let mailSends=document.querySelector(".welcome-message");
   mailSends.style.opacity = "1";
